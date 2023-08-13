4ba3b8ead846a9c0da3e4fd4c79580220ff4cb05
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-08-13 15:35:15 -0700

    Fix errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-/* 
- * This program prints the size of char, int, long int, and float types.
- * It returns "Size of a $var"
+/**                                                                                                                                                         
+ * main - entry point of the program                                                                                                                        
+ *                                                                                                                                                          
+ * Description: Prints: 'size of char, int, long int, and float types'                                                                                
+ *                                                                                                                                                          
+ * Return: Description                                                                                                                                      
  */
 int main(void)
 {
-// Print the size of char, int, long int, and float types
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 return (0);
