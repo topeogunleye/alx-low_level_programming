7fb1003c91369cb1d3bb12a073e2c8c4203845e0
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-08-13 15:11:30 -0700

    Fix errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - entry point of the program
+ *
+ * Description: prints a message: 'Programming is like building a multilingual puzzle'
+ *
+ * Return: Description
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
