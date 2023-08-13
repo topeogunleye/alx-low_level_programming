f79d336c2789298423c44e0216b1f40f69cf71e4
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-08-13 15:18:40 -0700

    Write a C program that prints exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description: with proper grammar, but the outcome is a piece of art
+ *
+ * Return: with proper grammar, but the outcome is a piece of art
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
