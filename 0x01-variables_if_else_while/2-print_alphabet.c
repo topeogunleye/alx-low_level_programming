a28ab1c1a6d93aa94e8a90990e57c628d73a7362
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-08-14 15:47:00 -0700

    Write a program in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,9 @@
+c = 'a'; /* initialize c with the first lowercase letter */
+while (c <= 'z') /* loop until the last lowercase letter */
+{
+    putchar(c); /* print the current letter */
+    c++; /* increment c to the next letter */
+}
+putchar('\n'); /* print a newline character */
+
+return (0);
